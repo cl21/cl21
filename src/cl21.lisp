c3a5e8dc5d17e7034f9713bc2d0f392ba9921f06
SANO Masatoshi <snmsts@gmail.com>
2014-01-25 03:49:05 +0900

    Fix error 'The function CL21.CORE.PACKAGE:IN-PACKAGE is undefined.' by sbcl.

--- src/cl21.lisp
+++ src/cl21.lisp
@@ -1,5 +1,5 @@
 (defpackage cl21)
-(in-package :cl21)
+(cl:in-package :cl21)
 
 (cl:dolist (package-name '(:cl21.core))
   (cl:let ((package (cl:find-package package-name)))
@@ -9,6 +9,6 @@
       (cl:shadowing-import symbol)
       (cl:export symbol))))
 
-(in-package :cl-user)
+(cl:in-package :cl-user)
 (defpackage cl21-user
   (:use :cl21))
