c8f2d73f20bd8b269318932466ffe6825f214c96
Eitarow Fukamachi <e.arrows@gmail.com>
2014-01-24 23:20:22 +0900

    Add re-match, re-replace and re-split.

--- src/re.lisp
+++ src/re.lisp
@@ -5,9 +5,30 @@
   (:import-from :cl21.core.readtable
                 :defreadtable
                 :in-readtable)
-  (:export :enable-cl21-re-syntax
+  (:import-from :cl-ppcre
+                :create-scanner
+                :scan-to-strings)
+  (:export :re-match
+           :re-replace
+           :re-split
+           :enable-cl21-re-syntax
            :disable-cl21-re-syntax))
 (in-package :cl21.re)
+
+(defun re-match (re string &rest keys &key start end)
+  (declare (ignore start end))
+  (apply re string keys))
+
+(defun re-replace (re string replacement &rest keys &key start end)
+  (declare (ignore start end))
+  (multiple-value-bind (scanner modifiers) (funcall re)
+    (if (find #\G modifiers)
+        (apply #'ppcre:regex-replace-all scanner string replacement keys)
+        (apply #'ppcre:regex-replace scanner string replacement keys))))
+
+(defun re-split (re target-string &rest keys &key start end limit)
+  (declare (ignore start end limit))
+  (apply #'ppcre:split (funcall re) target-string keys))
 
 (defun whitespacep (char)
   (member char '(#\Space #\Tab #\Newline #\Return #\Linefeed) :test #'char=))
@@ -34,9 +55,18 @@
 (defun regexp-reader (stream sub-char numarg)
   (declare (ignore numarg))
   (let* ((segment (segment-reader stream sub-char 1))
-         (modifiers (modifier-reader stream))
-         (re (regex-with-modifier (car segment) modifiers)))
-    `(lambda (str) (ppcre:scan-to-strings ,re str))))
+         (modifiers (modifier-reader stream)))
+    `(lambda (&optional str &rest keys)
+       (if str
+           (apply #'ppcre:scan-to-strings
+                  ,(regex-with-modifier (car segment) modifiers)
+                  str
+                  keys)
+           (values (ppcre:create-scanner
+                    ,(regex-with-modifier (car segment)
+                                          (and modifiers
+                                               (remove #\G (symbol-name modifiers)))))
+                   ,(coerce (symbol-name modifiers) 'simple-vector))))))
 
 (defreadtable (cl21-re-readtable :cl21.re)
   (:merge :standard)
