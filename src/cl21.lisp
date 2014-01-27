80abcc42c4097daf6cd99d0fee64993512af3571
Eitarow Fukamachi <e.arrows@gmail.com>
2014-01-28 00:40:44 +0900

    Prevent interning symbols into cl21 & cl21-user packages.

--- src/cl21.lisp
+++ src/cl21.lisp
@@ -1,13 +1,13 @@
 (defpackage cl21)
 (cl:in-package :cl21)
 
-(cl:dolist (package-name '(:cl21.core))
-  (cl:let ((package (cl:find-package package-name)))
-    (cl:unless package
-      (cl:error "Package \"~A\" doesn't exist." package-name))
-    (cl:do-external-symbols (symbol package)
-      (cl:shadowing-import (cl:list symbol))
-      (cl:export (cl:list symbol)))))
+(cl:dolist (#1=#:package-name '(:cl21.core))
+  (cl:let ((#2=#:package (cl:find-package #1#)))
+    (cl:unless #2#
+      (cl:error "Package \"~A\" doesn't exist." #1#))
+    (cl:do-external-symbols (#3=#:symbol #2#)
+      (cl:shadowing-import (cl:list #3#))
+      (cl:export (cl:list #3#)))))
 
 (cl:in-package :cl-user)
 (cl21::defpackage cl21-user
@@ -15,10 +15,10 @@
 (cl21::in-package :cl21-user)
 
 #+(or sbcl ccl clisp allegro ecl)
-(cl:do-external-symbols (symb
+(cl:do-external-symbols (#1=#:symb
                          #+sbcl    :sb-ext
                          #+ccl     :ccl
                          #+clisp   :ext
                          #+allegro :excl
                          #+ecl     :quit)
-  (cl:shadowing-import (cl:list symb)))
+  (cl:shadowing-import (cl:list #1#)))
