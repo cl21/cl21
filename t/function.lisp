05fe32e7a56e5604fefc2edcaaa3f1d1d21e2320
Eitarow Fukamachi <e.arrows@gmail.com>
2014-03-05 10:37:08 +0900

    (refs #35) Add tests for ^ syntax.

--- t/function.lisp
+++ t/function.lisp
@@ -4,7 +4,7 @@
         :cl-test-more))
 (in-package :cl21-test.function)
 
-(plan 7)
+(plan 12)
 
 (is (map (compose #'sin #'1+) '(1 2 3))
     '(0.9092974 0.14112 -0.7568025))
@@ -21,5 +21,23 @@
     '(0 1 3 5 7 9))
 (is (funcall #'(not zerop) 0.0)
     nil)
+
+(ok (map ^(random 10) (iota 10))
+    "no arguments")
+
+(is (map ^(coerce % 'list) (list #(1 2 3) "abc" #()))
+    '((1 2 3) (#\a #\b #\c) NIL)
+    "one argument")
+
+(ok (every ^(eql % %1)
+           (map ^(random 10) (iota 10)))
+    "% is an alias for %1")
+
+(is (sort '(2 7 6 5 2 0 6 0 9 9) ^(< %1 %2))
+    '(0 0 2 2 5 6 6 7 9 9)
+    "two arguments")
+
+(is (^%2 "Hello" "World") "World"
+    "ignore the first")
 
 (finalize)
